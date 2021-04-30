b9b71a0d92d37873f064253687930d675ebbdc4b
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-04-30 22:29:11 +0300

    Day-3

---
+++ oopIntro/src/oopIntroUpdate/CategoryManager.java
@@ -0,0 +1,11 @@
+package oopIntroUpdate;
+
+public class CategoryManager {
+
+	public void chooseCategory(Category category) {
+
+		System.out.println("Kategori seçme kodu çaðrýldý: " + category.getName());
+
+	}
+
+}
---
+++ oopIntro/src/oopIntroUpdate/ProductManager.java
@@ -0,0 +1,10 @@
+package oopIntroUpdate;
+
+public class ProductManager {
+
+	public void addToCart(Product product) {
+
+		System.out.println("Sepete ekleme kodu çaðrýldý: " + product.getName());
+
+	}
+}
