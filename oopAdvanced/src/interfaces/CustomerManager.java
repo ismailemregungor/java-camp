63ee4a5d98ec15e029b529e70b5b33db8828e8cf
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-05-02 06:31:07 +0300

    Day-4

---
+++ oopAdvanced/src/interfaces/CustomerManager.java
@@ -0,0 +1,29 @@
+package interfaces;
+
+public class CustomerManager {
+
+	
+	private Logger[] loggers;
+
+	public CustomerManager(Logger[] loggers) {
+		super();
+		this.loggers = loggers;
+	}
+
+	
+	public void add(Customer customer) {
+
+		System.out.println("Musteri Eklendi " + customer.getFirstName());
+		
+		Utils.runLoggers(loggers, customer.getFirstName());
+
+	}
+
+	public void delete(Customer customer) {
+
+		System.out.println("Musteri Silindi " + customer.getFirstName());
+		
+		Utils.runLoggers(loggers, customer.getFirstName());
+
+	}
+}
