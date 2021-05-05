32757eca5f4e37abff6e91b8b83191bf74a7d875
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-05-05 04:25:06 +0300

    Demo Class

---
+++ demos/src/demoClasses/CourseService.java
@@ -0,0 +1,18 @@
+package demoClasses;
+
+
+public class CourseService {
+
+	public void joinCourse(Course course) {
+
+		System.out.println(course.getName() + " kursuna katýldýnýz.");
+
+	}
+
+	public void leaveCourse(Course course) {
+
+		System.out.println(course.getName() + " kursundan ayrýldýnýz.");
+
+	}
+
+}
---
+++ demos/src/demoClasses/UserService.java
@@ -0,0 +1,17 @@
+package demoClasses;
+
+public class UserService {
+
+	public void createUser(User user) {
+
+		System.out.println(user.getName() + " kullanýcýsý oluþturuldu.");
+
+	}
+
+	public void changePassword(User user) {
+
+		System.out.println(user.getName() + " kullanýcýsýnýn þifresi baþarýyla deðiþtirildi.");
+
+	}
+	
+}
