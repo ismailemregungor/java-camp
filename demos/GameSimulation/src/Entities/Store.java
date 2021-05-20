92d88f04e83f2fe0fd7d62de9a1411a98447dab1
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-05-20 23:48:13 +0300

    Demo GameSimulation

---
+++ demos/GameSimulation/src/Entities/Store.java
@@ -0,0 +1,46 @@
+package Entities;
+
+import Abstract.Entity;
+
+public class Store implements Entity{
+	
+	private int id;
+	private String name;
+	private double unitPrice;
+	
+	public Store() {
+		
+	}
+
+	public Store(int id, String name, double unitPrice) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+}
